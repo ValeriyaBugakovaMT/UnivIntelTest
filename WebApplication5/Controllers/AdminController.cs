2b95005fa3d82ce7fdd47d2eb88bbf1e35530132
ValeriyaBugakovaMT <lerabugakova@outlook.com>
2019-05-30 14:34:40 +0600

    commit

---
+++ WebApplication5/Controllers/AdminController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Npgsql.EntityFrameworkCore.PostgreSQL;
+using WebApplication5.Models;
+
+namespace WebApplication5.Controllers
+{
+
+    public class AdminController : Controller
+    {
+        Project1 db = new Project1();
+        public ActionResult Index()
+        {
+            return View(db.DictCities.ToList());
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(DictCities book)
+        {
+            db.DictCities.Add(book);
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+        // GET: Admin
+     
+        public string addUser(string Title, string Code)
+        {
+            Guid id = Guid.NewGuid();
+            string text = Title;
+            string code = Code;
+            DictCities city = new DictCities { ID = id, Title = text, Code = code };
+            db.DictCities.Add(city);
+            
+            db.SaveChanges();
+            return "true";
+        }
+        public ActionResult showDictCities()
+        {
+            var model = db.DictCities.ToList();
+            return View(model);
+        }
+    }
+}
---
+++ WebApplication5/Models/DictCities.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication5.Models
+{
+    
+    public class Project1 : DbContext
+    {
+        public DbSet<DictCities> DictCities { get; set; }
+    }
+    [Table("DictCities", Schema = "public")]
+    public class DictCities : DbContext
+    {
+        [Column("Title")]
+        public string Title { get; set; }
+        [Column("Code")]
+        public string Code { get; set; }
+        [Column("id")]
+        public Guid ID { get; set; }
+    }
+
+}
